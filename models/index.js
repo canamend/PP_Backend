ac4e08bcb95758c367d2a101d801dd9ccc2ad64f
canamend <isaac.canamend@gmail.com>
2023-09-06 16:55:59 -0600

    Model of company and suppliers added to models index

--- models/index.js
+++ models/index.js
@@ -1,15 +1,20 @@
 
 const Category = require('./category');
+const Company = require('./company')
+const Product = require('./product')
 const Role = require('./role');
 const Server = require('./server');
+const Supplier = require('./supplier')
 const User = require('./user');
-const Product = require('./product')
+
 
 module.exports = {
     Category,
+    Company,
     Product,
     Role,
     Server,
+    Supplier,
     User,
 }
 
