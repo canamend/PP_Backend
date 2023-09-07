469ac02a3a81558096385a040852fe4e10c2a92b
canamend <isaac.canamend@gmail.com>
2023-09-07 14:31:27 -0600

    New helpers added to user routes

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,12 @@ const {
         hasRole,
         isAdminRole,
 } = require('../middlewares');
-const { isRoleValid, emailExists, userExistsById } = require('../helpers/db-validators');
+const { isRoleValid, emailExists, userExistsById, phoneExists, companyExistsById } = require('../helpers/db-validators');
 
 const Role = require('../models/role')
 const router = Router();
 
 const { usersGet, 
-        usersPatch, 
         usersPut, 
         usersPost, 
         usersDelete } = require('../controllers/users');
@@ -24,6 +23,9 @@ router.put('/:id', [
         check('id', 'No es un id válido').isMongoId(),
         check('id').custom( userExistsById ),
         check('role').custom( isRoleValid ),
+        check('email', 'El correo no es válido').isEmail(),
+        check('name', 'El nombre de la compañía tiene que tener al menos 4 caracteres').isLength({ min: 4 }),
+        check('phoneNumber').isMobilePhone('es-MX'),
         validarCampos
 ], usersPut);
 
@@ -34,19 +36,19 @@ router.post('/',[
         check('email').custom( emailExists ),
         check('role').custom( (role) => isRoleValid(role) ), //* El parametro de custom() puede ser únicamente el nombre de la función isRoleValid
                                                              //* ya que el callback y el argumento que se envía a la función son los mismos
+        check('phoneNumber').custom((phoneNumber) => phoneExists(phoneNumber, 'users') ),
+        check('phoneNumber').isMobilePhone('es-MX'),
+        check('company', 'No es un id válido').isMongoId(),
+        check('company').custom( companyExistsById ),
         validarCampos
 ], usersPost);
 
 router.delete('/:id', [
         validarJWT,
-        // isAdminRole,
-        hasRole('ADMIN_ROLE', 'VENTAS_ROLE'),
+        isAdminRole,
         check('id', 'No es un id válido').isMongoId(),
         check('id').custom( userExistsById ),
         validarCampos
 ], usersDelete);
-
-router.patch('/', usersPatch);
-
 
 module.exports = router;
